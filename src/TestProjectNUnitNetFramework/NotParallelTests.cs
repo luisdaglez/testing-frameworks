c46bcc06e744c53cc24d54f5325694194d2ec413
luisdaglez <7117564+luisdaglez@users.noreply.github.com>
2018-07-06 22:26:40 -0400

    adding not parallel and mixed parallel test classes/methods

---
+++ src/TestProjectNUnitNetFramework/NotParallelTests.cs
@@ -0,0 +1,22 @@
+﻿using ClassLibraryNetStandard;
+using NUnit.Framework;
+
+namespace TestProjectNUnitNetFramework
+{
+    [NonParallelizable]
+    class NotParallelTests
+    {
+        [Test]
+        public void NUnitSerialTest1NetFramework()
+        {
+            var foo = new Class1();
+            foo.Method1();
+        }
+        [Test]
+        public void NUnitSerialTest2NetFramework()
+        {
+            var foo = new Class1();
+            foo.Method1();
+        }
+    }
+}
