04b9d42fe5fc72503ad60b45dc074b30f36f13e2
luisdaglez <7117564+luisdaglez@users.noreply.github.com>
2018-07-06 23:42:09 -0400

    show case NUnit's setup and tear down at method, class, namespace and assembly levels

--- src/ClassLibraryNetStandard/Class1.cs
+++ src/ClassLibraryNetStandard/Class1.cs
@@ -1,9 +1,24 @@
-﻿using System.Threading;
+﻿using System;
+using System.Collections.Concurrent;
+using System.Diagnostics;
+using System.Reflection;
+using System.Threading;
 
 namespace ClassLibraryNetStandard
 {
     public class Class1
     {
-        public void Method1() => Thread.Sleep(5000);
+        public static ConcurrentDictionary<long, string> Dictionary = new ConcurrentDictionary<long, string>();
+
+        public void Method1()
+        {
+            var assembly = Assembly.GetCallingAssembly();
+
+            var stackTrace = new StackTrace();
+            // get calling method name
+            var name = stackTrace.GetFrame(1).GetMethod().Name;
+            Dictionary.TryAdd(DateTime.Now.Ticks, name + " " + assembly.FullName);
+            Thread.Sleep(5000);
+        }
     }
 }
